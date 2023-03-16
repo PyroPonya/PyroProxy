9e2cb23662539af32cc2bee25dfe80394b32eae9
PyroPonya <43729142+PyroPonya@users.noreply.github.com>
2023-03-16 13:40:17 +0300

    'Visitor-Id' header added

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ cors_proxy
   .createServer({
     originBlacklist: originBlacklist,
     originWhitelist: originWhitelist,
-    requireHeader: ['origin', 'x-requested-with', 'X-Client-Ip'],
+    requireHeader: ['origin', 'x-requested-with', 'X-Client-Ip', 'Visitor-Id'],
     checkRateLimit: checkRateLimit,
     removeHeaders: [
       'cookie',
